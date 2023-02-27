6213fa5f3badf45fcf49654e68e96ad09f149a67
cappellettiJP <juanpablocappelletti@gmail.com>
2023-02-27 20:51:52 -0300

    Initial commit

---
+++ src/main/java/com/dhc9pros/proyect1/Proyect1Application.java
@@ -0,0 +1,13 @@
+package com.dhc9pros.proyect1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyect1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyect1Application.class, args);
+	}
+
+}
